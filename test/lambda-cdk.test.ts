f6ce9612333950caa4f41fc4770a6efd99b15b7c
Alem Getu <alem.getu@raytheon.com>
2022-02-24 12:51:29 -0500

    remove test

--- test/lambda-cdk.test.ts
+++ test/lambda-cdk.test.ts
@@ -2,35 +2,33 @@ import { expect as expectCDK, haveResource, countResources } from '@aws-cdk/asse
 import * as cdk from '@aws-cdk/core';
 import { LamdaStackProps, LambdaCdkStack} from '../lib/lambda-cdk-stack';
 
-const testProps:LamdaStackProps = {
-  LambdaCdkConfig:{
-    application: "test-app",
-    lambdaRuntime: "NODEJS_14_X",
-    stageName: 'unit-test',
-    srcPath: '../lambda-function-repo/src',
-    functions: [
-      {
-        functionName: "test",
-        apiGateway: false,
-        handler: "index.main_one",
-        roleArn: "arn:aws:iam::010805440944:role/service-Role-ForLamdaCdk",
-        memorySize: 1024,
-        timeout: 30,
-        methods: ['GET']
-      }
-    ]
-  }
-}
+// const testProps:LamdaStackProps = {
+//  LambdaCdkConfig:{
+//    application: "test-app",
+//    lambdaRuntime: "NODEJS_14_X",
+//    stageName: 'unit-test',
+//    srcPath: '../lambda-function-repo/src',
+//    functions: [
+//      {
+//         functionName: "test",
+//         apiGateway: false,
+//         handler: "index.main_one",
+//         roleArn: "arn:aws:iam::010805440944:role/service-Role-ForLamdaCdk",
+//         memorySize: 1024,
+//         timeout: 30,
+//         methods: ['GET']
+//       }
+//     ]
+//   }
+// }
 test('Lambda Funtion', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new LambdaCdkStack(app, 'MyTestStack', testProps);
-    // THEN
-    // console.log(stack)
-    expectCDK(stack).to(countResources('AWS::Lambda::Function', 1));
-    // expectCDK(stack).to(haveResource("AWS::Lambda::Function",{
-    //   VisibilityTimeout: 600
-    // }));
-});
-
-
+//     const app = new cdk.App();
+//     // WHEN
+//     const stack = new LambdaCdkStack(app, 'MyTestStack', testProps);
+//     // THEN
+//     // console.log(stack)
+//     expectCDK(stack).to(countResources('AWS::Lambda::Function', 1));
+//     // expectCDK(stack).to(haveResource("AWS::Lambda::Function",{
+//     //   VisibilityTimeout: 600
+//     // }));
+}); 
